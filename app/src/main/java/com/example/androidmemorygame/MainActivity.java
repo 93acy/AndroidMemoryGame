dcf882134d3200ce2fec451fc9040fc571bdf6b7
Joel Yeo <joelyeo@outlook.com>
2021-07-12 22:55:40 +0800

    cross.png

--- app/src/main/java/com/example/androidmemorygame/MainActivity.java
+++ app/src/main/java/com/example/androidmemorygame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidmemorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //list/array of images
+
+        //list/array of buttons
+
+
+        //shuffle images
+
     }
 }
